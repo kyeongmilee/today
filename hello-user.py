87bcd343d4d985201b8d815b0197e5d26dff5b6b
kyeongmilee <41028604+kyeongmilee@users.noreply.github.com>
2018-07-17 13:41:49 +0900

    Create hello-user.py

---
+++ hello-user.py
@@ -0,0 +1,6 @@
+import datetime
+now = datetime.date.today()
+name = raw_input("what is your name~~??")
+
+s = "hello,["+name+"]! Today is [%02d/%02d/%04d]." % (now.month, now.day, now.year)
+print s
